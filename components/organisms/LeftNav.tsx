0b87aeb519df5a1872fb4aa14b771ac827d0a878
root <root@DESKTOP-MGOMRR6.localdomain>
2023-02-24 06:54:49 +0900

    amplify

--- components/organisms/LeftNav.tsx
+++ components/organisms/LeftNav.tsx
@@ -121,7 +121,6 @@ export const LeftNav = () => {
           <div className="ml-4">{t.leftNav.following}</div>
           {[
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0,
           ].map((sample, index) => {
             return (
               <div key={index}>
