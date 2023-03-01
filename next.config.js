9a2bc920b0f33b88fd1354685ccdfe3df145ae2f
root <root@DESKTOP-MGOMRR6.localdomain>
2023-03-02 05:52:35 +0900

    configure

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const TerserPlugin = require("terser-webpack-plugin");
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  reactStrictMode: true,
-  output: "standalone",
+  // reactStrictMode: true,
+  // output: "standalone",
   webpack: (config, options) => {
     config.optimization.minimize = isProd;
     config.optimization.minimizer = [
