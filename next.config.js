9230f8da9c06ece55bcc5a775f86a8a60fdfc423
root <root@DESKTOP-MGOMRR6.localdomain>
2023-03-02 18:05:43 +0900

    standalone false

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
-  outputStandalone: true,
-  experimental: {
-    outputStandalone: true,
-  },
   webpack: (config, options) => {
     config.optimization.minimize = isProd;
     config.optimization.minimizer = [
