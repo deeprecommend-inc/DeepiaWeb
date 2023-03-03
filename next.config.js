7395ab779c7bbd546e559f63dd9846d379a7e1f6
root <root@DESKTOP-MGOMRR6.localdomain>
2023-03-03 18:25:25 +0900

    f

--- next.config.js
+++ next.config.js
@@ -1,31 +1,25 @@
-// /** @type {import('next').NextConfig} */
-// const TerserPlugin = require('terser-webpack-plugin');
-// const isProd = process.env.NODE_ENV === 'production';
-
-// module.exports = {
-//     output: 'standalone',
-//     webpack: (config, options) => {
-//         config.optimization.minimize = isProd;
-//         config.optimization.minimizer = [
-//             new TerserPlugin({
-//                 terserOptions: {
-//                     compress: {
-//                         drop_console: isProd,
-//                     },
-//                 },
-//                 extractComments: 'all',
-//             }),
-//         ];
-//         return config;
-//     },
-//     i18n: {
-//         locales: ['en', 'ja'],
-//         defaultLocale: 'en',
-//     },
-// };
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
+const TerserPlugin = require('terser-webpack-plugin');
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig;
+module.exports = {
+    output: 'standalone',
+    webpack: (config, options) => {
+        config.optimization.minimize = isProd;
+        config.optimization.minimizer = [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: isProd,
+                    },
+                },
+                extractComments: 'all',
+            }),
+        ];
+        return config;
+    },
+    i18n: {
+        locales: ['en', 'ja'],
+        defaultLocale: 'en',
+    },
+};
