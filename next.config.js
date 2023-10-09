50505464450f4acba5d0ef47467cdd05364bcd09
jin-sugimoto-for-business <tatoonashi0203@icloud.com>
2023-10-10 08:08:26 +0900

    strict false

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     productionBrowserSourceMaps: false,
     output: 'standalone',
     webpack: (config, options) => {
