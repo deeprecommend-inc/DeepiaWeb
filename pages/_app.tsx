f44e53467d0ab1315c82b21cf6eb7f731594b2c8
jin-sugimoto-for-business <tatoonashi0203@icloud.com>
2023-10-15 21:35:29 +0900

    yarn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,10 @@ import { SessionProvider } from 'next-auth/react';
 import { useEffect } from 'react';
 import { accessTokenKey } from '../general/constants/localStorageKey';
 import { asyncLocalStorage } from '../general/utils/asyncLocalStorage';
-import { useRouter } from 'next/router';
 import ResponsiveDrawer from '../components/template/ResponsiveDrawer';
 import LoadingOverlay from '../components/template/LoadingOverlay';
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
-    const router = useRouter();
-
     return (
         <Provider store={store}>
             <SessionProvider session={session}>
